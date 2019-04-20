b973d497be8217f74852befc3a69fc31e7d149f6
Tom Gilbert <tgilbert09@homail.co.uk>
2019-04-20 19:53:34 +0800

    Added units to print outs

--- 3D Analysis/src/main.c
+++ 3D Analysis/src/main.c
@@ -11,7 +11,7 @@ int main (void) {
    double weight = 100*9.81;
    double span = 10;
    
-   printf("\nTotal span: %f\tCurrent Weight %f N\n", span, weight);
+   printf("\nTotal span: %fm\tCurrent Weight %fN\n", span, weight);
    
    // Pre processing of useful/repetitive variables
    int number_of_points = (int)(((span/2)*POINTS_PER_METRE)+1);
@@ -23,7 +23,7 @@ int main (void) {
    z = FUNC_spanning(dz, number_of_points);
    distributed_moment = FUNC_moment_distribution(weight, span, number_of_points, z);
    
-   printf("\nSpan Loc\tMoment\n");
+   printf("\nSpan Loc(m)\tMoment(Nm)\n");
 	for( i = 0; i < number_of_points; ++i ){
 	   printf("%f\t%f \n", z[i], distributed_moment[i]);
 	}
